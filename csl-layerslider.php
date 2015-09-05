363e2fc8ca2642fd171208b1bc7169000322ad94
William <hello@bigwilliam.com>
2015-09-05 05:53:15 -0700

    finish layerslider element

--- csl-layerslider.php
+++ csl-layerslider.php
@@ -4,9 +4,9 @@ Plugin Name: Cornerstone Library: Layerslider
 Plugin URI:  http://cornerstonelibrary.com/
 Description: This is a template plugin for creating new elements for Cornerstone
 Version:     0.1
-Author:      Your Name
-Author URI:  http://yourwebsite.com
-Author Email: youremail@example.com
+Author:      William Cobb
+Author URI:  http://bigwilliam.com
+Author Email: hello@bigwilliam.com
 Text Domain: __x__
 */
 
@@ -23,21 +23,21 @@ function csl_layerslider_scripts() {
 	wp_enqueue_script( 'csl-layerslider-script', plugins_url( '/assets/js/custom.js', __FILE__ ), array( 'jquery' ), null, true );
 	wp_enqueue_style( 'csl-layerslider-css', plugins_url( '/assets/css/custom.css', __FILE__ ), array(), '1.0' );
 }
-add_action( 'wp_enqueue_scripts', 'csl_layerslider_scripts', 100 );
+// add_action( 'wp_enqueue_scripts', 'csl_layerslider_scripts', 100 );
 
 
 /*
  * => Load Shortcodes
  * ---------------------------------------------------------------------------*/
-require_once('includes/shortcodes.php');
+// require_once('includes/shortcodes.php');
 
 /*
  * => ADD CUSTOM ELEMENTS TO CORNERSTONE
  * ---------------------------------------------------------------------------*/
 function csl_layerslider_elements() {
 	require_once( 'includes/layerslider-element.php' );
-	require_once( 'includes/layerslider-element-item.php' ); // include this if your item has a child type
+	// require_once( 'includes/layerslider-element-item.php' ); // include this if your item has a child type
   cornerstone_add_element( 'CSL_LayerSlider' );
-  cornerstone_add_element( 'CSL_LayerSlider_Item' );
+  // cornerstone_add_element( 'CSL_LayerSlider_Item' );
 }
 add_action( 'cornerstone_load_elements', 'csl_layerslider_elements' );
--- includes/layerslider-element.php
+++ includes/layerslider-element.php
@@ -22,58 +22,51 @@ class CSL_LayerSlider extends Cornerstone_Element_Base {
     return array(
       'name'        => 'csl-layerslider',
       'title'       => __( 'Layerslider', csl18n() ),
-      'section'     => 'content',
+      'section'     => 'media',
       'description' => __( 'Layerslider description.', csl18n() ),
-      'supports'    => array( 'id', 'class', 'style' ),
-      'childType'   => 'csl-layerslider-item',
-      'renderChild' => true
+      'supports'    => array( 'id', 'class', 'style' )
+      // 'childType'   => 'csl-layerslider-item',
+      // 'renderChild' => true
     );
   }
 
   public function controls() {
 
-    $this->addControl(
-      'elements', // control name
-      'sortable', // control type
-      __( 'Layerslider Items', csl18n() ), // Message
-      __( 'Add a new item to your Layerslider.', csl18n() ), // Tooltip text
-      array(
-        array( 'title' => __( 'Layerslider 1', csl18n() ), 'content' => __( 'Add some content to your Layerslider here.', csl18n() ) ),
-        array( 'title' => __( 'Layerslider 2', csl18n() ), 'content' => __( 'Add some content to your Layerslider here.', csl18n() ) )
-      ), // placeholder content or default items
-      array(
-        'newTitle' => __( 'Layerslider %s', csl18n() ),
-        'floor'    => 1
-      ) // new item format
-    );
+    $sliders = array();
 
-    $this->addSupport( 'id',
-      array( 'options' => array( 'monospace' => true ) )
+    if ( class_exists( 'LS_Sliders' ) ) {
+      $ls_sliders  = LS_Sliders::find();
+
+      foreach ( $ls_sliders as $slider ) {
+        $sliders[] = array( 'value' => $slider['id'], 'label' => $slider['name'] );
+      }
+    }
+
+    if ( empty( $sliders ) ) {
+      $sliders[] = array( 'value' => 'none', 'label' => __( 'No slider available', csl18n() ), 'disabled' => true );
+    }
+
+    $this->addControl(
+      'slide_id',
+      'select',
+      __( 'Select Slider', csl18n() ),
+      __( 'Choose from sliders that have already been created in the Layerslider admin area.', csl18n() ),
+      $sliders[0]['value'],
+      array(
+        'choices' => $sliders
+      )
     );
+  }
+
+  public function isActive() {
+    return class_exists( 'LS_Sliders' );
   }
 
   public function render( $atts ) {
 
     extract( $atts );
 
-    $contents = '';
-
-    foreach ( $elements as $e ) {
-
-      $item_extra = $this->extra( array(
-        'id'    => $e['id'],
-        'class' => $e['class'],
-        'style' => $e['style']
-      ) );
-
-      $e['parent_id'] = ( $link_items == 'true' && $id != '' ) ? $id : '';
-
-      $contents .= '[csl_layerslider_item title="' . $e['title'] . '" ';
-      $contents .= 'open="' . $e['open']  . '"' . $item_extra . ']' . $e['content'] . '[/csl_layerslider_item]';
-
-    }
-
-    $shortcode = "[csl_layerslider{$extra}]{$contents}[/csl_layerslider]";
+    $shortcode = "[layerslider id=\"$slide_id\"]";
 
     return $shortcode;
 
